3f15e7d391e26e32c89616d36532147901a9fd70
tiffany <yang.ding@hotmail.com>
2020-11-27 20:14:04 -0800

    added weather display, sub weather info, and some animation

--- script.js
+++ script.js
@@ -1,35 +1,84 @@
-// function to hit the API
-
-async function getWeather() {
-  try {
-    const response = await fetch(
-      'http://api.weatherapi.com/v1/forecast.json?key=1986480656ec490d950204923202611&q=guangzhou',
-      {
-        mode: 'cors',
-      }
-    );
-    const weatherData = await response.json();
-    console.log(weatherData);
-
-  } catch (error) {
-    alert(error);
+async function getWeatherData(location) {
+  const response = await fetch(
+    `http://api.weatherapi.com/v1/forecast.json?key=1986480656ec490d950204923202611&q=${location}`,
+    {
+      mode: 'cors',
+    }
+  );
+  if (response.status === 400) {
+    throwErrorMsg();
   }
+  const weatherData = await response.json();
+  const newData = processData(weatherData);
+  displayData(newData);
 }
 
-getWeather();
+function displayData(newData) {
+  const weatherInfo = document.getElementsByClassName('info');
+  Array.from(weatherInfo).forEach(div => {
+    div.classList.add('fade-in2');
+  })
 
-// function that takes a location and
-// return the weather data for that location
+  document.querySelector('.condition').textContent = newData.condition;
+  document.querySelector(
+    '.location'
+  ).textContent = `${newData.location}, ${newData.region}`;
+  document.querySelector('.degrees').textContent = newData.currentTemp.f;
+  document.querySelector(
+    '.feels-like'
+  ).textContent = `FEELS LIKE: ${newData.feelsLike.f}`;
+  document.querySelector('.wind-mph').textContent = `WIND: ${newData.wind} MPH`;
+  document.querySelector(
+    '.humidity'
+  ).textContent = `HUMIDITY: ${newData.humidity}`;
+}
 
-// function that process the JSON data you're getting
-// from the API
-// return an object with only the data you require for the app
+function processData(weatherData) {
+  // grab all the data i want to display on the page
+  const myData = {
+    condition: weatherData.current.condition.text,
+    feelsLike: {
+      f: Math.round(weatherData.current.feelslike_f),
+      c: Math.round(weatherData.current.feelslike_c),
+    },
+    currentTemp: {
+      f: Math.round(weatherData.current.temp_f),
+      c: Math.round(weatherData.current.temp_c),
+    },
+    wind: Math.round(weatherData.current.wind_mph),
+    humidity: weatherData.current.humidity,
+    location: weatherData.location.name.toUpperCase(),
+  };
 
-// set up simple form that let users input location and
-// fetch the weather info
+  // if in the US, add state
+  // if not, add country
+  if (weatherData.location.country === 'United States of America') {
+    myData['region'] = weatherData.location.region.toUpperCase();
+  } else {
+    myData['region'] = weatherData.location.country.toUpperCase();
+  }
 
-// display the information on your webpage
+  return myData;
+}
 
-// toggle between F and C degrees
+function throwErrorMsg() {
+  const error = document.querySelector('.error-msg');
+  error.style.display = 'block';
+}
 
-// OPTIONAL: loading component
+// get location from user
+function fetchWeather() {
+  const input = document.querySelector('input[type="text"]');
+  const userLocation = input.value;
+  getWeatherData(userLocation);
+}
+
+function handleSubmit(e) {
+  e.preventDefault();
+  fetchWeather();
+}
+
+const form = document.querySelector('form');
+const submitBtn = document.querySelector('.submit-btn');
+form.addEventListener('submit', handleSubmit);
+submitBtn.addEventListener('click', handleSubmit);
